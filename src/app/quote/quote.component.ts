a56f95b2e48dfdcec6bc575cb092f9fccfaa35f9
kev87ian <kelvinian87@gmail.com>
2020-03-31 19:46:21 +0300

    Made the quotes array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  quotes = [
 
-  ngOnInit(): void {
+    new Quote(1," The only serious question in life is whether to kill yourself or not.", "Albert Camus", "Kevin", 8,2,new Date(2019,2,22)), 
+    new Quote(2,"When the soul suffers too much, it develops a taste for misfortune.", "Albert Camus", "Kevin",0,0,new Date(2019,2,30)),
+   
+    ]
+  
+
+  
+  constructor() {
+  }
+
+  ngOnInit() {
   }
 
 }
