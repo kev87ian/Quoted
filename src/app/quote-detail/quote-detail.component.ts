c43558c21bf39d044e433501aa7fdb16bceffd03
kev87ian <kelvinian87@gmail.com>
2020-03-31 20:00:16 +0300

    add toggle and vote functionality

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote:Quote;
+
+  @Output() vote = new EventEmitter<boolean>();
+  @Output() downvote = new EventEmitter<boolean>();
+  @Output() quoteDelete = new EventEmitter<boolean>();
+
+  deleteQuote(complete:boolean){
+    this.quoteDelete.emit(complete);
+  }
+
+  upVote(countVote:boolean){
+    this.vote.emit(countVote);
+  }
+  downVote(countDownVote:boolean){
+    this.downvote.emit(countDownVote);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
