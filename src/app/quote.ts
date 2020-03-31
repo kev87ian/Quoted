f328d81468cc28037ddc869e0761b456edd267f3
kev87ian <kelvinian87@gmail.com>
2020-03-31 19:37:28 +0300

    Add quotes object

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+    
+    public showDesc:boolean;
+    constructor(public id:number, public quote:string,public author:string, public publisher:string,public upvotes:number,public downVote:number,public completeDate: Date){
+        this.showDesc=false
+    }
+    
 }
